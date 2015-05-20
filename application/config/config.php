5cc49993bff87113c5c53db7980f3359b18abf73
hiveship <maelig.nantel@gmail.com>
2015-05-20 16:38:39 +0200

    config

--- application/config/config.php
+++ application/config/config.php
@@ -18,7 +18,7 @@ defined( 'BASEPATH' ) or exit( 'No direct script access allowed' );
  * |
  */
 $config ['base_url'] = 'http://localhost:8888/PHP/enssat-serveur-voeux/';
-// todo
+
 /*
  * |--------------------------------------------------------------------------
  * | Index File
