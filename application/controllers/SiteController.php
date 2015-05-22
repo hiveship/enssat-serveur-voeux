86ef3f1f8a6b76a8dc982be4805082f6e6c95849
hiveship <maelig.nantel@gmail.com>
2015-05-22 23:51:32 +0200

    login form

--- application/controllers/SiteController.php
+++ application/controllers/SiteController.php
@@ -9,6 +9,7 @@ class SiteController extends CI_Controller
 		$this -> load -> library( 'session' );
 		$this -> load -> helper( 'url' );
 		$this -> load -> helper( 'form' );
+		$this -> load -> helper( 'html' );
 		$this -> load -> library( 'form_validation' );
 		$this -> load -> model( 'enseignant/EnseignantModel' );
 	}
@@ -20,18 +21,27 @@ class SiteController extends CI_Controller
 
 	public function login ()
 	{
-		$this -> form_validation -> set_rules( 'login', 'login', 'required' );
-		$this -> form_validation -> set_rules( 'password', 'password', 'required' );
+		$this -> form_validation -> set_message( 'required', 'Ce champ doit être renseigné.' );
 		
-		$login = $this -> input -> post( 'login' );
-		$pasword = $this -> input -> post( 'password' );
-		$me = $this -> EnseignantModel -> get( $login, $password );
-		if ( $me != FALSE ) {
-			ApplicationController::flash_error( "Vous êtes désormais connecté !" );
-			// redirect('page voulue');
+		$this -> form_validation -> set_rules( 'login', 'Login doit être renseigné', 'trim|required' );
+		$this -> form_validation -> set_rules( 'password', 'password', 'trim|required' );
+		if ( $this -> form_validation -> run() === FALSE ) {
+			$this -> load -> view( 'site/login' );
 		} else {
-			ApplicationController::flash_error( "Erreur d'authentification !" );
-			redirect( 'site/login' );
+			$login = $this -> input -> post( 'login', true );
+			$password = $this -> input -> post( 'password' );
+			echo "<br/>";
+			echo "login vaut : " . $login;
+			echo "<br/>";
+			echo "password vaut : " . $password;
+			$me = $this -> EnseignantModel -> get( $login, $password );
+			if ( $me != FALSE ) {
+				// ApplicationController::flash_error( "Vous êtes désormais connecté !" );
+				// redirect('page voulue');
+			} else {
+				// ApplicationController::flash_error( "Erreur d'authentification !" );
+				// redirect( 'site/login' );
+			}
 		}
 	}
 
--- application/views/site/login.php
+++ application/views/site/login.php
@@ -11,20 +11,40 @@
 </head>
 
 <body>
-	<div class="page-header">
-		<center>
-			<h2>Espace réservé, veillez vous identifier</h2>
-		</center>
+	<div class="container">
+		<div class="row login-form well col-centered col-md-3">
+			<?php
+			
+			echo form_open( 'SiteController/login', 'role="form"' );
+			$login = array ( 
+				
+					'id' => 'login', 
+					'name' => 'login',
+					/* En cas d'erreur, à la validation du formulaire, si le champs était rempli, la saisie de l'utilisateur sera toujours présente.*/
+					'value' => set_value( 'login' ) 
+			);
+			echo form_label( 'Login: ', 'login', 'class="icon-white icon-user"' );
+			echo form_input( $login, '', 'class="form-control"' );
+			echo form_error( 'login' );
+			
+			$password = array ( 
+				
+					'name' => 'password', 
+					'id' => 'password', 
+					'value' => set_value( 'password' ) 
+			);
+			echo form_label( 'Password: ', 'password', 'class="icon-white icon-question-sign"' );
+			echo form_password( $password, 'password', 'class="form-control"' );
+			echo form_error( 'password' );
+			
+			echo br( 1 );
+			echo form_submit( 'submit', 'Connexion', 'class="btn btn-primary pull-right"' );
+			
+			echo form_close();
+			?>
+		</div>
 	</div>
 
-	<form role="form">
-		<div class="span12 row ">
-			<label for="email">Login:</label> <input type="email" class="form-control" id="login">
-		</div>
-		<div class="form-group span12 row">
-			<label for="pwd">Password:</label> <input type="password" class="form-control" id="password">
-		</div>
-		<button type="submit" class="btn btn-default">Connexion</button>
-	</form>
+
 </body>
 </html>
