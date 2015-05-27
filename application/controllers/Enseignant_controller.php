fc53d51610e4e0cad99d29822748a59eed03931c
NunusPower <anais.galisson@gmail.com>
2015-05-27 17:25:51 +0200

    Moncompte 50% fait + deconnexion

--- application/controllers/Enseignant_controller.php
+++ application/controllers/Enseignant_controller.php
@@ -1,11 +1,23 @@
 <?php
 
-class Enseignant_controller extends ApplicationController
+include "Application_controller.php";
+
+class Enseignant_controller extends Application_controller
 {
 
 	public function __construct ()
 	{
-		parent::__construct();
+		parent::__construct ();
+		$this -> load -> model ( 'enseignant/Enseignant_model' );
+	}
+
+	public function get ()
+	{
+		$data ['user'] = $this -> Enseignant_model -> get_user ()[0];
+		$data ['title'] = 'Mon Compte';
+		
+		$this -> load -> template ( 'user/moncompte', $data );
+	
 	}
 
 }
--- application/models/enseignant/Enseignant_model.php
+++ application/models/enseignant/Enseignant_model.php
@@ -5,32 +5,41 @@ class Enseignant_model extends CI_Model
 
 	public function __construct ()
 	{
-		$this -> load -> database();
+		parent::__construct ();
+		$this -> load -> database ();
 	}
 
 	public function get ( $login, $password )
 	{
-		$query = $this -> db -> get_where( 'enseignant', array ( 
-			
+		$query = $this -> db -> get_where ( 'enseignant', array ( 
+				
 				'login' => $login, 
 				'pwd' => $password 
 		), 1 );
-		$result = $query -> row();
-		if ( empty( $result ) ) {
+		$result = $query -> row ();
+		if ( empty ( $result ) ) {
 			return FALSE;
 		} else {
-			return $this -> create_user( $result );
+			return $this -> create_user ( $result );
 		}
+	}
+
+	public function get_user ()
+	{
+		$me = $this -> session -> userdata ( 'me' );
+		$login = $me ['login'];
+		$query = $this -> db -> query ( "SELECT * FROM `enseignant` WHERE login= '" . $login . "'" );
+		return $query -> result_array ();
 	}
 
 	private function create_user ( $resultRow )
 	{
-		if ( empty( $resultRow ) ) {
+		if ( empty ( $resultRow ) ) {
 			return FALSE;
 		} else {
 			// Créer une classe Utilisateur ??
 			$user = array ( 
-				
+					
 					'login' => $resultRow -> login, 
 					'password' => $resultRow -> pwd, 
 					'nom' => $resultRow -> nom, 
