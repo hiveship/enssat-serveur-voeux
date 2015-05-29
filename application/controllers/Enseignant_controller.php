19a00b109bb6f9c7ad970b89bf7105681e4935d1
NunusPower <anais.galisson@gmail.com>
2015-05-29 13:08:20 +0200

    Signed-off-by: NunusPower <anais.galisson@gmail.com>

--- application/controllers/Enseignant_controller.php
+++ application/controllers/Enseignant_controller.php
@@ -7,31 +7,36 @@ class Enseignant_controller extends Application_controller
 
 	public function __construct ()
 	{
-		parent::__construct();
-		$this -> load -> model( 'enseignant/Enseignant_model' );
+		parent::__construct ();
+		$this -> load -> model ( 'enseignant/Enseignant_model' );
 	}
 
 	public function get ( $login )
 	{
-		flash_info( "id récupéré à partir de l'url vaut : " . $login );
-		$this -> load -> template( 'enseignants/edit', $this -> session -> userdata( 'me' ) );
+		flash_info ( "id récupéré à partir de l'url vaut : " . $login );
+		$this -> load -> template ( 'enseignants/edit', $this -> session -> userdata ( 'me' ) );
 	}
 
 	public function index ()
 	{
 		$data = array ( 
-			
-				'enseignants' => $this -> Enseignant_model -> get_all() 
+				
+				'enseignants' => $this -> Enseignant_model -> get_all () 
 		);
-		$this -> load -> template( 'enseignants/index', $data );
+		$this -> load -> template ( 'enseignants/index', $data );
 	}
 
 	public function rendre_administrateur ( $login )
 	{
 		// TODO controls
-		$this -> Enseignant_model -> rendre_administrateur( $login );
-		flash_success( "L'utilisateur a bien été nommé administrateur !" );
-		redirect( "Enseignant_controller/index", "refresh" );
+		$this -> Enseignant_model -> rendre_administrateur ( $login );
+		flash_success ( "L'utilisateur a bien été nommé administrateur !" );
+		redirect ( "Enseignant_controller/index", "refresh" );
+	}
+
+	public function change_password ()
+	{
+	
 	}
 
 }
--- application/views/enseignants/edit.php
+++ application/views/enseignants/edit.php
@@ -2,42 +2,52 @@
 <div class="container">
 	<h2>Mon Compte</h2>
 
+	<p><?php echo $nom . " " . $prenom; ?></p>
+	<p> Statut: <?php echo $statut; ?></p>
+	<p> Statutaire: <?php echo $statutaire; ?></p>
+	<p> Compte actif :
+
+	<?php
+	if ( $actif ) {
+		echo "<INPUT type='checkbox' name='actif' value='actif' disabled='disabled' checked>";
+	} else {
+		echo "<INPUT type='checkbox' name='actif' value='inactif' disabled='disabled'>";
+	}
+	?>
+</p>
+	<p> Compte administrateur :
+
 <?php
-echo "<p>";
-echo $nom . " " . $prenom;
-echo "</p>";
 
-echo "<p>";
-echo "Statut : " . $statut;
-echo "</p>";
-echo "<p>";
-echo "Statutaire : " . $statutaire;
-echo "</p>";
-
-echo "<p>";
-
-/**
- * * Actif **
- */
-echo "Compte actif : ";
-if ( $actif ) {
-	echo "<INPUT type='checkbox' name='actif' value='actif' disabled='disabled' checked>";
-} else {
-	echo "<INPUT type='checkbox' name='actif' value='inactif' disabled='disabled'>";
-}
-echo "</p>";
-
-/**
- * * Admin ***
- */
-echo "<p>";
-echo "Compte administrateur : ";
-echo $administrateur;
 if ( $administrateur ) {
 	echo "<INPUT type='checkbox' name='administrateur' value='administrateur' disabled='disabled' checked>";
 } else {
 	echo "<INPUT type='checkbox' name='administrateur' value='enseignant' disabled='disabled'>";
 }
-echo "</p>";
+
 ?>
+</p>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </div>
