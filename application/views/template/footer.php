e17509cbe01e794b5a7496bf4a9c3c6b019a1a43
4r3 <4r3gn4ult@gmail.com>
2015-06-19 10:53:19 +0200

    repair

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -6,10 +6,13 @@
 		src="<?php echo base_url("assets/js/tests/gremlins.min.js"); ?>"></script>
 
 	<script>
+	//decommenter pour lacher la horde
+	
+	
+	//$("a[href='<?php echo site_url('logout') ?>']").attr('href', '#')
 	gremlins.createHorde()
 	  .gremlin(gremlins.species.formFiller())
 	  .gremlin(gremlins.species.clicker().clickTypes(['click']))
-	  .gremlin(gremlins.species.scroller())
 	  //.unleash();
 	</script>
 
