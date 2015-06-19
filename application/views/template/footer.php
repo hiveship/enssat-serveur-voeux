b124f2b19315a49d75df6e578edf1e60d5c92359
4r3 <4r3gn4ult@gmail.com>
2015-06-19 10:31:57 +0200

    commit repair

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,11 +2,16 @@
 </body>
 <footer>
 
-	<script src="<?php echo base_url("assets/js/tests/gremlins.min.js"); ?>"></script>
+	<script
+		src="<?php echo base_url("assets/js/tests/gremlins.min.js"); ?>"></script>
 
 	<script>
-//gremlins.createHorde().unleash();
-</script>
+	gremlins.createHorde()
+	  .gremlin(gremlins.species.formFiller())
+	  .gremlin(gremlins.species.clicker().clickTypes(['click']))
+	  .gremlin(gremlins.species.scroller())
+	  //.unleash();
+	</script>
 
 </footer>
 </html>
