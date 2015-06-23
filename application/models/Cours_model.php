1798ab6213722346863708f971a97d5c77add6aa
4r3 <4r3gn4ult@gmail.com>
2015-06-23 11:33:13 +0200

    doc

--- application/models/Cours_model.php
+++ application/models/Cours_model.php
@@ -6,14 +6,26 @@ class Cours_model extends CI_model
 
 	public function __construct ()
 	{
-		parent::__construct();
-		$this -> load -> database();
+		parent::__construct ();
+		$this -> load -> database ();
 	}
 
+	/**
+	 * permet de creer une nouvelle partie d'un module
+	 * 
+	 * @param string $id
+	 *        	identifiant du module de la partie
+	 * @param string $part
+	 *        	nom de la partie
+	 * @param string $type
+	 *        	type de la partie
+	 * @param string $hed
+	 *        	volume horaire de la partie
+	 */
 	public function create ( $id, $part, $type, $hed )
 	{
-		$this -> db -> insert( 'contenu', array ( 
-			
+		$this -> db -> insert ( 'contenu', array ( 
+				
 				'module' => $id, 
 				'partie' => $part, 
 				'type' => $type, 
@@ -21,45 +33,71 @@ class Cours_model extends CI_model
 		) );
 	}
 
+	/**
+	 * permet de supprimer la partie identifiée par le couple module/partie
+	 * la partie n'est pas renseignée alors la fonction supprime toute les parties du module désigné
+	 * 
+	 * @param string $module
+	 *        	identifiant du module
+	 * @param string $partie
+	 *        	identifiant de la partie
+	 */
 	public function delete ( $module, $partie = NULL )
 	{
 		if ( $partie == NULL ) {
-			$this -> db -> delete( self::TABLE_NAME, array ( 
-				
+			$this -> db -> delete ( self::TABLE_NAME, array ( 
+					
 					'module' => $module 
 			) );
 		} else {
-			$this -> db -> delete( self::TABLE_NAME, array ( 
-				
+			$this -> db -> delete ( self::TABLE_NAME, array ( 
+					
 					'module' => $module, 
 					'partie' => $partie 
 			) );
 		}
 	}
 
+	/**
+	 * permet de recuperer un tableau contenant les donnée de la partie identifiée par le couple module/partie
+	 * la partie n'est pas renseignée alors la fonction revoie toute les parties du module désigné
+	 * 
+	 * @param string $module
+	 *        	identifiant du module
+	 * @param string $partie
+	 *        	identifiant de la partie
+	 */
 	public function get ( $module, $partie = NULL )
 	{
 		if ( $partie == NULL ) {
-			$querry = $this -> db -> get_where( self::TABLE_NAME, array ( 
-				
+			$querry = $this -> db -> get_where ( self::TABLE_NAME, array ( 
+					
 					'module' => $module 
 			) );
 		} else {
-			$querry = $this -> db -> get_where( self::TABLE_NAME, array ( 
-				
+			$querry = $this -> db -> get_where ( self::TABLE_NAME, array ( 
+					
 					'module' => $module, 
 					'partie' => $partie 
 			) );
 		}
-		return $querry -> result_array();
+		return $querry -> result_array ();
 	}
-
+	// TODO finir doc
+	/**
+	 *
+	 * @param unknown $module        	
+	 * @param unknown $partie_old        	
+	 * @param unknown $partie_new        	
+	 * @param unknown $type        	
+	 * @param unknown $hed        	
+	 */
 	public function update ( $module, $partie_old, $partie_new, $type, $hed )
 	{
-		$this -> db -> where( 'module', $module );
-		$this -> db -> where( 'partie', $partie_old );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'module', $module );
+		$this -> db -> where ( 'partie', $partie_old );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'partie' => $partie_new, 
 				'type' => $type, 
 				'hed' => $hed 
@@ -68,69 +106,69 @@ class Cours_model extends CI_model
 
 	public function inscrire_enseignant ( $module, $partie, $enseignant )
 	{
-		$this -> db -> where( 'module', $module );
-		$this -> db -> where( 'partie', $partie );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'module', $module );
+		$this -> db -> where ( 'partie', $partie );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'enseignant' => $enseignant 
 		) );
 	}
 
 	public function desinscrire_enseignant ( $module, $partie, $enseignant )
 	{
-		$this -> db -> where( 'module', $module );
-		$this -> db -> where( 'partie', $partie );
-		$this -> db -> where( 'enseignant', $enseignant );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'module', $module );
+		$this -> db -> where ( 'partie', $partie );
+		$this -> db -> where ( 'enseignant', $enseignant );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'enseignant' => NULL 
 		) );
 	}
 
 	public function desinscrire_enseignant_tout ( $enseignant )
 	{
-		$this -> db -> where( 'enseignant', $enseignant );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'enseignant', $enseignant );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'enseignant' => NULL 
 		) );
 	}
 
 	public function get_modules_id_de ( $enseignant )
 	{
-		$this -> db -> where( 'enseignant', $enseignant );
-		$this -> db -> order_by( "module", "asc" );
-		$this -> db -> select( 'module' );
-		$this -> db -> distinct();
-		$querry = $this -> db -> get( self::TABLE_NAME );
-		return $querry -> result_array();
+		$this -> db -> where ( 'enseignant', $enseignant );
+		$this -> db -> order_by ( "module", "asc" );
+		$this -> db -> select ( 'module' );
+		$this -> db -> distinct ();
+		$querry = $this -> db -> get ( self::TABLE_NAME );
+		return $querry -> result_array ();
 	}
 
 	public function get_cours_de ( $enseignant, $module = NULL )
 	{
 		if ( $module != NULL ) {
-			$this -> db -> where( 'module', $module );
+			$this -> db -> where ( 'module', $module );
 		}
-		$this -> db -> where( 'enseignant', $enseignant );
-		$querry = $this -> db -> get( self::TABLE_NAME );
-		return $querry -> result_array();
+		$this -> db -> where ( 'enseignant', $enseignant );
+		$querry = $this -> db -> get ( self::TABLE_NAME );
+		return $querry -> result_array ();
 	}
 
 	public function exists ( $module, $partie )
 	{
-		$this -> db -> where( 'module', $module );
-		$this -> db -> where( 'partie', $partie );
-		$query = $this -> db -> get( self::TABLE_NAME );
-		return $query -> num_rows() == 1;
+		$this -> db -> where ( 'module', $module );
+		$this -> db -> where ( 'partie', $partie );
+		$query = $this -> db -> get ( self::TABLE_NAME );
+		return $query -> num_rows () == 1;
 	}
 
 	public function est_libre ( $module, $partie )
 	{
-		$this -> db -> where( 'enseignant', null );
-		$this -> db -> where( 'module', $module );
-		$this -> db -> where( 'partie', $partie );
-		$query = $this -> db -> get( self::TABLE_NAME );
-		return $query -> num_rows() == 1;
+		$this -> db -> where ( 'enseignant', null );
+		$this -> db -> where ( 'module', $module );
+		$this -> db -> where ( 'partie', $partie );
+		$query = $this -> db -> get ( self::TABLE_NAME );
+		return $query -> num_rows () == 1;
 	}
 
 }
--- application/models/Module_model.php
+++ application/models/Module_model.php
@@ -7,87 +7,144 @@ class Module_model extends CI_model
 
 	public function __construct ()
 	{
-		parent::__construct();
-		$this -> load -> database();
+		parent::__construct ();
+		$this -> load -> database ();
 	}
 
+	/**
+	 * la fonction va inserer dans la base un nouveau module a condition qu'aucun autre module ait le meme nom
+	 * 
+	 * @param string $nom
+	 *        	nom
+	 *        	du module
+	 * @param string $public
+	 *        	public
+	 *        	visé par le module
+	 * @param string $semestre
+	 *        	semsetre
+	 *        	du module
+	 * @param string $libelle
+	 *        	libelle
+	 *        	du module
+	 * @return NULL si echec de creation, l'id du module sinon
+	 */
 	public function create ( $nom, $public, $semestre, $libelle )
 	{
 		$data = array ( 
-			
+				
 				'nom' => $nom, 
 				'public' => $public, 
 				'semestre' => $semestre, 
 				'libelle' => $libelle 
 		);
 		
-		$res = $this -> get_nom( $nom );
+		$res = $this -> get_nom ( $nom );
 		
-		if ( sizeof( $res ) == 0 ) {
-			$this -> db -> insert( self::TABLE_NAME, $data );
-			return $this -> get_nom( $nom );
+		if ( sizeof ( $res ) == 0 ) {
+			$this -> db -> insert ( self::TABLE_NAME, $data );
+			return $this -> get_nom ( $nom );
 		} else {
 			return null;
 		}
 	}
 
+	/**
+	 *
+	 * @return tableau contenant tout les modules de la base
+	 */
 	public function get_all ()
 	{
-		$querry = $this -> db -> get( self::TABLE_NAME );
-		return $querry -> result_array();
+		$querry = $this -> db -> get ( self::TABLE_NAME );
+		return $querry -> result_array ();
 	}
 
+	/**
+	 *
+	 * @param string $ID
+	 *        	identifiant du module a recuperer
+	 * @return tableau contenant tout le module avec l'id transmis en parametre
+	 */
 	public function get ( $ID )
 	{
-		$querry = $this -> db -> get_where( self::TABLE_NAME, array ( 
-			
+		$querry = $this -> db -> get_where ( self::TABLE_NAME, array ( 
+				
 				'id' => $ID 
 		) );
-		return $querry -> result_array()[0];
+		return $querry -> result_array ()[0];
 	}
 
+	/**
+	 *
+	 * @param string $nom
+	 *        	nom du module a recuperer
+	 * @return tableau contenant tout le module avec l'id transmis en parametre
+	 */
 	public function get_nom ( $nom )
 	{
-		$this -> db -> select( 'id' );
-		$querry = $this -> db -> get_where( self::TABLE_NAME, array ( 
-			
+		$this -> db -> select ( 'id' );
+		$querry = $this -> db -> get_where ( self::TABLE_NAME, array ( 
+				
 				'nom' => $nom 
 		) );
-		return $querry -> result_array()[0];
+		return $querry -> result_array ()[0];
 	}
 
+	/**
+	 *
+	 * @param string $responsable        	
+	 * @return tableau contenant tout les module ou le responsable transmis en parametre est inscrit
+	 */
 	public function get_modules_id_de ( $responsable )
 	{
-		$this -> db -> where( 'responsable', $responsable );
-		$this -> db -> order_by( "id", "asc" );
-		$this -> db -> select( 'id' );
-		$this -> db -> distinct();
-		$querry = $this -> db -> get( self::TABLE_NAME );
-		return $querry -> result_array();
+		$this -> db -> where ( 'responsable', $responsable );
+		$this -> db -> order_by ( "id", "asc" );
+		$this -> db -> select ( 'id' );
+		$this -> db -> distinct ();
+		$querry = $this -> db -> get ( self::TABLE_NAME );
+		return $querry -> result_array ();
 	}
 
+	/**
+	 *
+	 * @param string $ID
+	 *        	supprime le module avec l'identifiant transmis en parametre
+	 */
 	public function delete ( $ID )
 	{
-		$this -> db -> delete( 'contenu', array ( 
-			
+		$this -> db -> delete ( 'contenu', array ( 
+				
 				'module' => $ID 
 		) );
 		
-		$this -> db -> delete( self::TABLE_NAME, array ( 
-			
+		$this -> db -> delete ( self::TABLE_NAME, array ( 
+				
 				'id' => $ID 
 		) );
 	}
 
+	/**
+	 *
+	 * @param string $ID
+	 *        	id du module a modifier
+	 * @param string $nom
+	 *        	nouveau nom
+	 * @param string $public
+	 *        	nouveau public
+	 * @param string $semestre
+	 *        	nouveau semestre
+	 * @param string $libelle
+	 *        	nouveau libelle
+	 * @return boolean
+	 */
 	public function update ( $ID, $nom, $public, $semestre, $libelle )
 	{
-		if ( ! $this -> exists( $ID ) ) {
+		if ( ! $this -> exists ( $ID ) ) {
 			return false; // FIXME cette vérification doit être faite dans le controlleur, pas dans le modèle
 		}
 		
-		$this -> db -> where( 'id', $ID );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'id', $ID );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'nom' => $nom, 
 				'public' => $public, 
 				'semestre' => $semestre, 
@@ -96,46 +153,80 @@ class Module_model extends CI_model
 		return true;
 	}
 
+	/**
+	 *
+	 * @param string $ID
+	 *        	id a verifier
+	 * @return boolean retourne vrai si le module existe faux sinon
+	 */
 	public function exists ( $ID )
 	{
-		$this -> db -> select( 'id' );
-		$this -> db -> where( 'id', $ID );
-		$query = $this -> db -> get( self::TABLE_NAME );
-		return $query -> num_rows() == 1;
+		$this -> db -> select ( 'id' );
+		$this -> db -> where ( 'id', $ID );
+		$query = $this -> db -> get ( self::TABLE_NAME );
+		return $query -> num_rows () == 1;
 	}
 
+	/**
+	 *
+	 * @param string $ID
+	 *        	id a verifier
+	 * @return boolean retourne vrai si le module n'a pas de responsable faux sinon
+	 */
 	public function est_libre ( $ID )
 	{
-		$this -> db -> where( 'responsable', null );
-		$this -> db -> where( 'id', $ID );
-		$query = $this -> db -> get( self::TABLE_NAME );
-		return $query -> num_rows() == 1;
+		$this -> db -> where ( 'responsable', null );
+		$this -> db -> where ( 'id', $ID );
+		$query = $this -> db -> get ( self::TABLE_NAME );
+		return $query -> num_rows () == 1;
 	}
 
+	/**
+	 * permet d'inscrire un responsable dans un module
+	 * 
+	 * @param string $ID
+	 *        	id du module ou inscrire le responsable
+	 * @param string $enseignant
+	 *        	login du responssable
+	 */
 	public function inscrire_responsable ( $ID, $enseignant )
 	{
-		$this -> db -> where( 'id', $ID );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'id', $ID );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'responsable' => $enseignant 
 		) );
 	}
 
+	/**
+	 * permet de desinscrire un responsable dans un module
+	 * 
+	 * @param string $ID
+	 *        	id du module ou inscrire le responsable
+	 * @param string $enseignant
+	 *        	login du responssable
+	 */
 	public function desinscrire_responsable ( $ID, $enseignant )
 	{
-		$this -> db -> where( 'id', $ID );
-		$this -> db -> where( 'responsable', $enseignant );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'id', $ID );
+		$this -> db -> where ( 'responsable', $enseignant );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'responsable' => NULL 
 		) );
 	}
 
+	/**
+	 * permet de desinscrire un responsable de tout ses modules
+	 * 
+	 * @param string $enseignant
+	 *        	login du responssable
+	 */
 	public function desinscrire_responsable_tout ( $enseignant )
 	{
-		$this -> db -> where( 'responsable', $enseignant );
-		$this -> db -> update( self::TABLE_NAME, array ( 
-			
+		$this -> db -> where ( 'responsable', $enseignant );
+		$this -> db -> update ( self::TABLE_NAME, array ( 
+				
 				'responsable' => NULL 
 		) );
 	}
