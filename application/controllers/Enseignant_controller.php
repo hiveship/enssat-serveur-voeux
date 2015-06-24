b08354dd73e7bdba87dac68d50d3289527a4613c
4r3 <4r3gn4ult@gmail.com>
2015-06-24 18:40:29 +0200

    refactor + doc

--- application/controllers/Enseignant_controller.php
+++ application/controllers/Enseignant_controller.php
@@ -7,11 +7,11 @@ class Enseignant_controller extends Application_controller
 
 	public function __construct ()
 	{
-		parent::__construct();
-		$this -> load -> model( 'Enseignant_model' );
-		$this -> load -> model( 'Cours_model' );
-		$this -> load -> model( 'Module_model' );
-		$this -> load -> model( 'Decharge_model' );
+		parent::__construct ();
+		$this -> load -> model ( 'Enseignant_model' );
+		$this -> load -> model ( 'Cours_model' );
+		$this -> load -> model ( 'Module_model' );
+		$this -> load -> model ( 'Decharge_model' );
 	}
 
 	/**
@@ -20,16 +20,16 @@ class Enseignant_controller extends Application_controller
 	public function edit ()
 	{
 		$data = array ( 
-			
-				'me' => $this -> session -> userdata( 'me' ), 
-				'total_decharges' => $this -> Decharge_model -> get_sum( $this -> session -> userdata( 'me' )['login'] ), 
-				'enseigne' => $this -> Enseignant_model -> get_sum_heures( $this -> session -> userdata( 'me' )['login'] ), 
-				'sum_cm' => $this -> Enseignant_model -> get_sum_cm( $this -> session -> userdata( 'me' )['login'] ), 
-				'sum_td' => $this -> Enseignant_model -> get_sum_td( $this -> session -> userdata( 'me' )['login'] ), 
-				'sum_tp_projet' => $this -> Enseignant_model -> get_sum_tp( $this -> session -> userdata( 'me' )['login'] ), 
-				'sum_ds' => $this -> Enseignant_model -> get_sum_ds( $this -> session -> userdata( 'me' )['login'] ) 
+				
+				'me' => $this -> session -> userdata ( 'me' ), 
+				'total_decharges' => $this -> Decharge_model -> get_sum ( $this -> session -> userdata ( 'me' )['login'] ), 
+				'enseigne' => $this -> Enseignant_model -> get_sum_heures ( $this -> session -> userdata ( 'me' )['login'] ), 
+				'sum_cm' => $this -> Enseignant_model -> get_sum_cm ( $this -> session -> userdata ( 'me' )['login'] ), 
+				'sum_td' => $this -> Enseignant_model -> get_sum_td ( $this -> session -> userdata ( 'me' )['login'] ), 
+				'sum_tp_projet' => $this -> Enseignant_model -> get_sum_tp ( $this -> session -> userdata ( 'me' )['login'] ), 
+				'sum_ds' => $this -> Enseignant_model -> get_sum_ds ( $this -> session -> userdata ( 'me' )['login'] ) 
 		);
-		$this -> load -> template( 'enseignants/edit', $data );
+		$this -> load -> template ( 'enseignants/edit', $data );
 	}
 
 	/**
@@ -37,9 +37,9 @@ class Enseignant_controller extends Application_controller
 	 */
 	public function index ()
 	{
-		$this -> load -> template( 'enseignants/index', array ( 
-			
-				'enseignants' => $this -> Enseignant_model -> get_all() 
+		$this -> load -> template ( 'enseignants/index', array ( 
+				
+				'enseignants' => $this -> Enseignant_model -> get_all () 
 		) );
 	}
 
@@ -49,7 +49,7 @@ class Enseignant_controller extends Application_controller
 	 */
 	public function get ()
 	{
-		echo json_encode( $this -> Enseignant_model -> get( $this -> session -> userdata( 'me' )['login'] ) );
+		echo json_encode ( $this -> Enseignant_model -> get ( $this -> session -> userdata ( 'me' )['login'] ) );
 	}
 
 	/**
@@ -59,34 +59,34 @@ class Enseignant_controller extends Application_controller
 	public function change_password ()
 	{
 		// Règles du formulaire
-		$this -> form_validation -> set_rules( 'password', 'Mot de passe (actuel)', 'required' );
-		$this -> form_validation -> set_rules( 'newpassword1', 'Nouveau mot de passe', 'required' );
-		$this -> form_validation -> set_rules( 'newpassword2', 'Nouveau mot de passe (confirmation)', 'required' );
+		$this -> form_validation -> set_rules ( 'password', 'Mot de passe (actuel)', 'required' );
+		$this -> form_validation -> set_rules ( 'newpassword1', 'Nouveau mot de passe', 'required' );
+		$this -> form_validation -> set_rules ( 'newpassword2', 'Nouveau mot de passe (confirmation)', 'required' );
 		
-		if ( $this -> form_validation -> run() == FALSE ) {
-			flash_error( "Veuillez recommencer, vous n'avez pas rempli le formulaire correctement." );
+		if ( $this -> form_validation -> run () == FALSE ) {
+			flash_error ( "Veuillez recommencer, vous n'avez pas rempli le formulaire correctement." );
 		} else {
 			// Récupère les variables du formulaire
-			$newpwd1 = $this -> input -> post( 'newpassword1' );
-			$newpwd2 = $this -> input -> post( 'newpassword2' );
-			$password = $this -> input -> post( 'password' );
+			$newpwd1 = $this -> input -> post ( 'newpassword1' );
+			$newpwd2 = $this -> input -> post ( 'newpassword2' );
+			$password = $this -> input -> post ( 'password' );
 			
-			if ( ! $this -> Enseignant_model -> verify_password( $password, $this -> session -> userdata( 'me' )['password'] ) ) {
-				flash_error( "Le mot de passe renseigné et le mot de passe actuel ne correspondent pas !" );
+			if ( ! $this -> Enseignant_model -> verify_password ( $password, $this -> session -> userdata ( 'me' )['password'] ) ) {
+				flash_error ( "Le mot de passe renseigné et le mot de passe actuel ne correspondent pas !" );
 			} else if ( $newpwd1 != $newpwd2 ) {
-				flash_error( "Les deux nouveaux mots de passes ne sont pas identiques !" );
+				flash_error ( "Les deux nouveaux mots de passes ne sont pas identiques !" );
 			} else {
 				// Tout est ok, on procède à la modification.
-				$newSecurePassword = $this -> Enseignant_model -> update_password( $this -> session -> userdata( 'me' )['login'], $newpwd1 );
+				$newSecurePassword = $this -> Enseignant_model -> update_password ( $this -> session -> userdata ( 'me' )['login'], $newpwd1 );
 				
 				// On doit également modifier le contenu des informations de l'utilisateur courant (stockées en session)
-				$me = $this -> session -> userdata( 'me' );
+				$me = $this -> session -> userdata ( 'me' );
 				$me ['password'] = $newSecurePassword;
-				$this -> session -> set_userdata( 'me', $me );
-				flash_success( "Votre mot de passe a bien été modifié !" );
+				$this -> session -> set_userdata ( 'me', $me );
+				flash_success ( "Votre mot de passe a bien été modifié !" );
 			}
 		}
-		redirect( site_url( 'enseignants/edit' ) );
+		redirect ( site_url ( 'enseignants/edit' ) );
 	}
 
 	/**
@@ -94,37 +94,37 @@ class Enseignant_controller extends Application_controller
 	 */
 	public function update ()
 	{
-		$this -> form_validation -> set_rules( 'email', 'Email', 'required|valid_email' );
-		$this -> form_validation -> set_rules( 'nom', 'Nom', 'required' );
-		$this -> form_validation -> set_rules( 'prenom', 'Prenom', 'required' );
-		$this -> form_validation -> set_rules( 'statutaire', 'Statutaire', 'required' );
+		$this -> form_validation -> set_rules ( 'email', 'Email', 'required|valid_email' );
+		$this -> form_validation -> set_rules ( 'nom', 'Nom', 'required' );
+		$this -> form_validation -> set_rules ( 'prenom', 'Prenom', 'required' );
+		$this -> form_validation -> set_rules ( 'statutaire', 'Statutaire', 'required' );
 		
-		if ( $this -> form_validation -> run() == FALSE ) {
-			flash_error( "Erreur dans votre formulaire, le changement n'a pu etre effectué" );
+		if ( $this -> form_validation -> run () == FALSE ) {
+			flash_error ( "Erreur dans votre formulaire, le changement n'a pu etre effectué" );
 		} else {
-			$login = $this -> session -> userdata( 'me' )['login'];
-			$nom = $this -> input -> post( 'nom' );
-			$prenom = $this -> input -> post( 'prenom' );
-			$email = $this -> input -> post( 'email' );
-			$statutaire = $this -> input -> post( 'statutaire' );
+			$login = $this -> session -> userdata ( 'me' )['login'];
+			$nom = $this -> input -> post ( 'nom' );
+			$prenom = $this -> input -> post ( 'prenom' );
+			$email = $this -> input -> post ( 'email' );
+			$statutaire = $this -> input -> post ( 'statutaire' );
 			
 			// Update dans la base de donnée - TODO optimiser car trop de requêtes à la BDD que l'on peut regrouperh
-			$this -> Enseignant_model -> update_prenom( $login, $prenom );
-			$this -> Enseignant_model -> update_nom( $login, $nom );
-			$this -> Enseignant_model -> update_email( $login, $email );
-			$this -> Enseignant_model -> update_statutaire( $login, $statutaire );
+			$this -> Enseignant_model -> update_prenom ( $login, $prenom );
+			$this -> Enseignant_model -> update_nom ( $login, $nom );
+			$this -> Enseignant_model -> update_email ( $login, $email );
+			$this -> Enseignant_model -> update_statutaire ( $login, $statutaire );
 			
 			// Update dans la session (pour avoir les infos en direct sans avoir à se déconnecter avant)
-			$me = $this -> session -> userdata( 'me' );
+			$me = $this -> session -> userdata ( 'me' );
 			$me ['nom'] = $nom;
 			$me ['prenom'] = $prenom;
 			$me ['email'] = $email;
 			$me ['statutaire'] = $statutaire;
-			$this -> session -> set_userdata( 'me', $me );
+			$this -> session -> set_userdata ( 'me', $me );
 			
-			flash_success( "Votre compte a bien été mis à jour." );
+			flash_success ( "Votre compte a bien été mis à jour." );
 		}
-		redirect( site_url( 'enseignants/edit' ) );
+		redirect ( site_url ( 'enseignants/edit' ) );
 	}
 
 	/**
@@ -136,38 +136,38 @@ class Enseignant_controller extends Application_controller
 	public function cours_de ( $login = null )
 	{
 		if ( $login == null ) {
-			$login = $this -> session -> userdata( 'me' )['login'];
+			$login = $this -> session -> userdata ( 'me' )['login'];
 		}
-		if ( ! $this -> Enseignant_model -> exists( $login ) ) { // Si on fournit un login inconnue... Par exemple modification du paramètre passé en GET dans l'URL.
-			redirect( 'Module_controller', auto );
+		if ( ! $this -> Enseignant_model -> exists ( $login ) ) { // Si on fournit un login inconnue... Par exemple modification du paramètre passé en GET dans l'URL.
+			redirect ( 'cours', auto );
 		}
 		
 		$Ids = array ();
-		$modIds = $this -> Cours_model -> get_modules_id_de( $login );
+		$modIds = $this -> Cours_model -> get_modules_id_de ( $login );
 		foreach ( $modIds as $mod ) {
-			array_push( $Ids, $mod ['module'] );
+			array_push ( $Ids, $mod ['module'] );
 		}
 		
-		$modIds = $this -> Module_model -> get_modules_id_de( $login );
+		$modIds = $this -> Module_model -> get_modules_id_de ( $login );
 		foreach ( $modIds as $mod ) {
-			if ( ! array_search( $mod ['id'], $Ids ) ) {
-				array_push( $Ids, $mod ['id'] );
+			if ( ! array_search ( $mod ['id'], $Ids ) ) {
+				array_push ( $Ids, $mod ['id'] );
 			}
 		}
 		
 		$modules = array ();
 		$cours = array ();
 		foreach ( $Ids as $Id ) {
-			array_push( $modules, $this -> Module_model -> get( $Id ) );
-			array_push( $cours, $this -> Cours_model -> get_cours_de( $login, $Id ) );
+			array_push ( $modules, $this -> Module_model -> get ( $Id ) );
+			array_push ( $cours, $this -> Cours_model -> get_cours_de ( $login, $Id ) );
 		}
 		
 		$data = array ( 
-			
+				
 				'cours' => $cours, 
 				'modules' => $modules 
 		);
-		$this -> load -> template( 'cours/get', $data );
+		$this -> load -> template ( 'cours/get', $data );
 	}
 
 	/**
@@ -176,42 +176,42 @@ class Enseignant_controller extends Application_controller
 	public function inscrire ( $module, $cours = null )
 	{
 		if ( $cours == null ) {
-			if ( ! $this -> Module_model -> est_libre( $module ) ) {
-				flash_error( "il y a déja un responsable ou le module n'exite pas" );
-				redirect( 'Module_controller', 'auto' ); // TODO renomer avec le nom de la bonne route
+			if ( ! $this -> Module_model -> est_libre ( $module ) ) {
+				flash_error ( "il y a déja un responsable ou le module n'exite pas" );
+				redirect ( 'cours', 'auto' ); // TODO renomer avec le nom de la bonne route
 			}
-			$this -> Module_model -> inscrire_responsable( $module, $this -> session -> userdata( 'me' )['login'] );
+			$this -> Module_model -> inscrire_responsable ( $module, $this -> session -> userdata ( 'me' )['login'] );
 		} else {
-			$cours = rawurldecode( $cours ); // à cause des espaces
-			if ( ! $this -> Cours_model -> est_libre( $module, $cours ) ) {
-				flash_error( "cours occupé ou non existant " . $cours );
-				redirect( 'Module_controller', 'auto' ); // TODO renomer avec le nom de la bonne route
+			$cours = rawurldecode ( $cours ); // à cause des espaces
+			if ( ! $this -> Cours_model -> est_libre ( $module, $cours ) ) {
+				flash_error ( "cours occupé ou non existant " . $cours );
+				redirect ( 'cours', 'auto' ); // TODO renomer avec le nom de la bonne route
 			}
-			$this -> Cours_model -> inscrire_enseignant( $module, $cours, $this -> session -> userdata( 'me' )['login'] );
+			$this -> Cours_model -> inscrire_enseignant ( $module, $cours, $this -> session -> userdata ( 'me' )['login'] );
 			// TODO un flash_success de confirmation ?
 		}
-		redirect( 'Module_controller', 'auto' ); // TODO renomer avec le nom de la bonne route
+		redirect ( 'cours', 'auto' ); // TODO renomer avec le nom de la bonne route
 	}
 
 	public function retirer ( $module, $cours = null )
 	{
 		if ( $cours == null ) {
-			$this -> Module_model -> desinscrire_responsable( $module, $this -> session -> userdata( 'me' )['login'] );
+			$this -> Module_model -> desinscrire_responsable ( $module, $this -> session -> userdata ( 'me' )['login'] );
 			// TODO message success
 		} else {
-			$cours = rawurldecode( $cours ); // à cause des espaces
-			$this -> Cours_model -> desinscrire_enseignant( $module, $cours, $this -> session -> userdata( 'me' )['login'] );
+			$cours = rawurldecode ( $cours ); // à cause des espaces
+			$this -> Cours_model -> desinscrire_enseignant ( $module, $cours, $this -> session -> userdata ( 'me' )['login'] );
 			// TODO message success
 		}
-		redirect( 'Module_controller', 'auto' ); // TODO renomer avec le nom de la bonne route
+		redirect ( 'cours', 'auto' ); // TODO renomer avec le nom de la bonne route
 	}
 
 	public function get_heures_effectue ( $login = null )
 	{
 		if ( $login == null ) {
-			$login = $this -> session -> userdata( 'me' )['login'];
+			$login = $this -> session -> userdata ( 'me' )['login'];
 		}
-		echo $this -> Enseignant_model -> get_sum_heures( $login );
+		echo $this -> Enseignant_model -> get_sum_heures ( $login );
 	}
 	
 	// TODO devrait plutôt se trouver dans me modèle étant donné que c'est une règle métier
--- application/controllers/admin/Cours_controller.php
+++ application/controllers/admin/Cours_controller.php
@@ -1,5 +1,5 @@
 <?php
-$dir = dirname( __FILE__ );
+$dir = dirname ( __FILE__ );
 include ( $dir . '/Admin_controller.php' );
 
 class Cours_controller extends Admin_controller
@@ -7,33 +7,38 @@ class Cours_controller extends Admin_controller
 
 	public function __construct ()
 	{
-		parent::__construct();
-		$this -> load -> model( 'Module_model' );
-		$this -> load -> model( 'Cours_model' );
+		parent::__construct ();
+		$this -> load -> model ( 'Module_model' );
+		$this -> load -> model ( 'Cours_model' );
 	}
 
+	/**
+	 *
+	 * @param string $module        	
+	 * @param string $partie        	
+	 */
 	public function edit ( $module, $partie )
 	{
-		$partie = rawurldecode( $partie );
-		$this -> check_parameters( $module, $partie );
+		$partie = rawurldecode ( $partie );
+		$this -> check_parameters ( $module, $partie );
 		
-		$this -> form_validation -> set_rules( 'nom', 'nom', 'trim|required' );
-		$this -> form_validation -> set_rules( 'type', 'type', 'trim|required' );
-		$this -> form_validation -> set_rules( 'hed', 'hed', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'nom', 'nom', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'type', 'type', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'hed', 'hed', 'trim|required' );
 		
-		if ( $this -> form_validation -> run() === TRUE ) {
-			$partie_new = $this -> input -> post( 'nom' );
-			$type = $this -> input -> post( 'type' );
-			$hed = $this -> input -> post( 'hed' );
-			if ( $partie != $partie_new && $this -> Cours_model -> exists( $module, $partie_new ) ) {
-				flash_error( "nom de module déja alloué ! " );
-				redirect( 'admin/cours/edit/' . $module . '/' . $partie, 'auto' );
+		if ( $this -> form_validation -> run () === TRUE ) {
+			$partie_new = $this -> input -> post ( 'nom' );
+			$type = $this -> input -> post ( 'type' );
+			$hed = $this -> input -> post ( 'hed' );
+			if ( $partie != $partie_new && $this -> Cours_model -> exists ( $module, $partie_new ) ) {
+				flash_error ( "nom de module déja alloué ! " );
+				redirect ( 'admin/cours/edit/' . $module . '/' . $partie, 'auto' );
 			}
-			$this -> Cours_model -> update( $module, $partie, $partie_new, $type, $hed );
-			redirect( 'admin/cours', 'auto' );
+			$this -> Cours_model -> update ( $module, $partie, $partie_new, $type, $hed );
+			redirect ( 'admin/cours', 'auto' );
 		}
 		
-		$this -> load -> template( 'admin/cours/edit', $this -> Cours_model -> get( $module, $partie )[0] );
+		$this -> load -> template ( 'admin/cours/edit', $this -> Cours_model -> get ( $module, $partie )[0] );
 	}
 
 	/**
@@ -44,53 +49,53 @@ class Cours_controller extends Admin_controller
 	public function create ( $module = NULL )
 	{
 		if ( $module == NULL ) {
-			redirect( 'admin/cours', 'auto' );
+			redirect ( 'admin/cours', 'auto' );
 		}
 		
 		$modules = array ( 
-			
+				
 				'module' => $module 
 		);
 		
-		$this -> form_validation -> set_rules( 'nom', 'nom', 'trim|required' );
-		$this -> form_validation -> set_rules( 'type', 'type', 'trim|required' );
-		$this -> form_validation -> set_rules( 'hed', 'hed', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'nom', 'nom', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'type', 'type', 'trim|required' );
+		$this -> form_validation -> set_rules ( 'hed', 'hed', 'trim|required' );
 		
-		if ( $this -> form_validation -> run() === TRUE ) {
-			$part = $this -> input -> post( 'nom' );
-			$type = $this -> input -> post( 'type' );
-			$hed = $this -> input -> post( 'hed' );
-			if ( ! $this -> Cours_model -> exists( $module, $part ) ) {
-				$this -> Cours_model -> create( $module, $part, $type, $hed );
-				redirect( 'admin/cours', 'auto' );
+		if ( $this -> form_validation -> run () === TRUE ) {
+			$part = $this -> input -> post ( 'nom' );
+			$type = $this -> input -> post ( 'type' );
+			$hed = $this -> input -> post ( 'hed' );
+			if ( ! $this -> Cours_model -> exists ( $module, $part ) ) {
+				$this -> Cours_model -> create ( $module, $part, $type, $hed );
+				redirect ( 'admin/cours', 'auto' );
 			} else {
-				flash_error( "la partie existe déja" );
+				flash_error ( "la partie existe déja" );
 			}
 		}
 		
-		$this -> load -> template( 'admin/cours/create', $modules );
+		$this -> load -> template ( 'admin/cours/create', $modules );
 	}
 
 	public function delete ( $module, $partie )
 	{
-		$partie = rawurldecode( $partie );
-		$this -> check_parameters( $module, $partie );
-		$this -> Cours_model -> delete( $module, $partie );
-		redirect( 'admin/cours', 'auto' );
+		$partie = rawurldecode ( $partie );
+		$this -> check_parameters ( $module, $partie );
+		$this -> Cours_model -> delete ( $module, $partie );
+		redirect ( 'admin/cours', 'auto' );
 	}
 
 	public function get_ajax ( $module, $partie )
 	{
-		$partie = rawurldecode( $partie );
-		$this -> check_parameters( $module, $partie );
-		echo json_encode( $this -> Cours_model -> get( $module, $partie ) );
+		$partie = rawurldecode ( $partie );
+		$this -> check_parameters ( $module, $partie );
+		echo json_encode ( $this -> Cours_model -> get ( $module, $partie ) );
 	}
 
 	private function check_parameters ( $module, $partie )
 	{
-		if ( ! isset( $partie ) || ! isset( $module ) || ! $this -> Cours_model -> exists( $module, $partie ) ) {
-			flash_error( "Vous devez spécifier un ID valide ! : recu -> " . $module . "  " . $partie );
-			redirect( 'admin/cours', 'auto' );
+		if ( ! isset ( $partie ) || ! isset ( $module ) || ! $this -> Cours_model -> exists ( $module, $partie ) ) {
+			flash_error ( "Vous devez spécifier un ID valide ! : recu -> " . $module . "  " . $partie );
+			redirect ( 'admin/cours', 'auto' );
 		}
 	}
 
